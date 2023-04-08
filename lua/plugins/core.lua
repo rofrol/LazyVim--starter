8429f269ece3ddb2ab26070da1a4005b4a006bc5
Roman Frołow <rofrol@gmail.com>
2023-04-08 06:40:43 +0200

    :Telescope notify like in astrovim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -15,5 +15,9 @@ return {
       -- https://github.com/alpha2phi/modern-neovim/blob/eb688331b2f44b2fa35a5f26d7856322b92d1e53/lua/plugins/init.lua#L41
       background_colour = "#1a1b26",
     },
+    -- https://github.com/evertonlopesc/nvim/blob/6480813d7cbe08fbf915164ad4d247075ccf41e1/lua/plugins/bootstrap.lua#L47
+    config = function()
+      require("telescope").load_extension("notify")
+    end,
   },
 }
