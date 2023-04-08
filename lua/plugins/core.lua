2297e1bed74e873fcfd85bb9d5d13213adae7417
Roman Frołow <rofrol@gmail.com>
2023-04-08 23:24:51 +0200

    notify and background_color fix correctly

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -10,14 +10,17 @@ return {
   },
   {
     "rcarriga/nvim-notify",
-    opts = {
-      -- https://www.reddit.com/r/neovim/comments/z7aqw0/comment/j0pe3ln/
-      -- https://github.com/alpha2phi/modern-neovim/blob/eb688331b2f44b2fa35a5f26d7856322b92d1e53/lua/plugins/init.lua#L41
-      background_colour = "#1a1b26",
-    },
     -- https://github.com/evertonlopesc/nvim/blob/6480813d7cbe08fbf915164ad4d247075ccf41e1/lua/plugins/bootstrap.lua#L47
     config = function()
       require("telescope").load_extension("notify")
+      -- https://github.com/ecosse3/nvim/blob/cbe9028039658591210bf1d44ffa41605a8b6f69/lua/config/plugins.lua#L323
+      require("notify").setup({
+        -- https://github.com/rcarriga/nvim-notify/issues/188
+        -- https://www.reddit.com/r/neovim/comments/z7aqw0/comment/j0pe3ln/
+        -- https://github.com/alpha2phi/modern-neovim/blob/eb688331b2f44b2fa35a5f26d7856322b92d1e53/lua/plugins/init.lua#L41
+        -- setting this in opts doesn't work
+        background_colour = "#ff0000",
+      })
     end,
   },
   {
