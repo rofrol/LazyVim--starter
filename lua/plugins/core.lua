37e5d589ff3636eb3060faac73832f38f70203af
Roman Frołow <rofrol@gmail.com>
2023-04-08 04:18:39 +0200

    notify and background

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -8,4 +8,12 @@ return {
       opts.ignore_install = { "help" }
     end,
   },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      -- https://www.reddit.com/r/neovim/comments/z7aqw0/comment/j0pe3ln/
+      -- https://github.com/alpha2phi/modern-neovim/blob/eb688331b2f44b2fa35a5f26d7856322b92d1e53/lua/plugins/init.lua#L41
+      background_colour = "#1a1b26",
+    },
+  },
 }
