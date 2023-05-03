b1e8ed873d222ef4a088ac20e52fd489afe859ec
Roman Frołow <rofrol@gmail.com>
2023-05-03 21:40:18 +0200

    delete other buffers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.swapfile = false
+
+-- delete other buffers
+-- https://tech.serhatteker.com/post/2021-04/vim-delete-multiple-buffers/
+-- https://neovim.io/doc/user/lua-guide.html#lua-guide-mappings-set
+vim.keymap.set("n", "<Leader>do", "<cmd>%bdelete|edit#|bdelete#<cr>")
