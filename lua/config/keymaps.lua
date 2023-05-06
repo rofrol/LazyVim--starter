119719256b22f2bcd854092c110fadeba9a50674
Roman Frołow <rofrol@gmail.com>
2023-05-06 21:56:39 +0200

    keymaps.lua: noh, new function map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,19 @@
 -- Add any additional keymaps here
 
 -- https://vi.stackexchange.com/questions/39149/how-to-stop-neovim-from-yanking-text-on-pasting-over-selection/39907#39907
-local function map(m, k, v)
-  vim.keymap.set(m, k, v, { silent = true })
+-- local function map(m, k, v)
+--   vim.keymap.set(m, k, v, { silent = true })
+-- end
+
+-- https://blog.devgenius.io/create-custom-keymaps-in-neovim-with-lua-d1167de0f2c2
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 map("v", "p", "P", { noremap = true, silent = true })
+
+map("n", "<Backspace>", "<cmd>noh<cr>", { noremap = true, silent = true })
