fd025d61eeb957d185073055d1f66d5001a3898a
Roman Frołow <rofrol@gmail.com>
2023-05-23 10:30:15 +0200

    akinsho/toggleterm.nvim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -43,4 +43,16 @@ return {
       },
     },
   },
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        shade_terminals = false,
+        -- add --login so ~/.zprofile is loaded
+        -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+        shell = "zsh --login",
+      })
+    end,
+  },
 }
