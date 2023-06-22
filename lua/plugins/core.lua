47b39df6bfadca9f51088a66a013303ca20464e7
Roman Frołow <rofrol@gmail.com>
2023-06-22 13:29:47 +0200

    cleaning

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -22,13 +22,10 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = { window = {
-      position = "right",
-    } },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        position = "right",
+      },
       filesystem = {
         filtered_items = {
           visible = true,
