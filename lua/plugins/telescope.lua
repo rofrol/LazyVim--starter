79def495fc134896698979ad9968ff8ad727efac
Roman Frołow <rofrol@gmail.com>
2023-06-28 14:28:42 +0200

    harpoon

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -65,4 +65,114 @@ return {
       },
     },
   },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "ThePrimeagen/harpoon",
+      -- https://github.com/albohlabs/dotfiles/blob/2597a6d0529e5b3dd0e4b71eea3985a68a75608d/nvim/.config/nvim/lua/plugins/harpoon.lua
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+      },
+      config = function()
+        require("harpoon").setup()
+        require("telescope").load_extension("harpoon")
+      end,
+      -- https://github.com/Fymyte/nvim-config/blob/c9e56fe7216cdfcaa019d537b1bb1c79407b30c6/lua/fymyte/plugins/editing-tools.lua#L105
+      keys = {
+        {
+          "<leader>a",
+          function()
+            require("harpoon.mark").add_file()
+          end,
+          desc = "[A]dd harpoon mark",
+        },
+        {
+          "<leader>h",
+          function()
+            require("telescope").extensions.harpoon.marks()
+          end,
+          desc = "[S]earch h[A]rpoon mark",
+        },
+        {
+          "<leader>j",
+          function()
+            require("harpoon.ui").toggle_quick_menu()
+          end,
+          desc = "[H]arpoon [T]oogle quick menu",
+        },
+        {
+          "<leader>1",
+          function()
+            require("harpoon.ui").nav_file(1)
+          end,
+          desc = "Harpoon go to file 1",
+        },
+        {
+          "<leader>2",
+          function()
+            require("harpoon.ui").nav_file(2)
+          end,
+          desc = "Harpoon go to file 2",
+        },
+        {
+          "<leader>3",
+          function()
+            require("harpoon.ui").nav_file(3)
+          end,
+          desc = "Harpoon go to file 3",
+        },
+        {
+          "<leader>4",
+          function()
+            require("harpoon.ui").nav_file(4)
+          end,
+          desc = "Harpoon go to file 4",
+        },
+        {
+          "<leader>5",
+          function()
+            require("harpoon.ui").nav_file(5)
+          end,
+          desc = "Harpoon go to file 5",
+        },
+        {
+          "<leader>6",
+          function()
+            require("harpoon.ui").nav_file(6)
+          end,
+          desc = "Harpoon go to file 6",
+        },
+        {
+          "<leader>7",
+          function()
+            require("harpoon.ui").nav_file(7)
+          end,
+          desc = "Harpoon go to file 7",
+        },
+        {
+          "<leader>8",
+          function()
+            require("harpoon.ui").nav_file(8)
+          end,
+          desc = "Harpoon go to file 8",
+        },
+        {
+          "<leader>9",
+          function()
+            require("harpoon.ui").nav_file(9)
+          end,
+          desc = "Harpoon go to file 9",
+        },
+        {
+          "C-.",
+          function()
+            require("harpoon.mark").move_mark_up()
+          end,
+          desc = "Harpoon move mark up",
+        },
+        -- map("n", "<c-n>", move_mark_down)
+      },
+    },
+  },
 }
