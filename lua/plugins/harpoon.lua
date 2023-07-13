4016a45ed3ee536cf59bcf0898b241422852b4e6
Roman Frołow <rofrol@gmail.com>
2023-07-13 10:20:36 +0200

    harpoon: menu width

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -14,7 +14,7 @@ return {
       require("harpoon").setup({
         tabline = true,
         menu = {
-          width = vim.api.nvim_win_get_width(0) - 10,
+          width = vim.api.nvim_win_get_width(0) - 20,
         },
       })
     end,
