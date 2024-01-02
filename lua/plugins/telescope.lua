6736b7f027fb7feaa167878053882cbb78979865
Roman Frołow <rofrol@gmail.com>
2024-01-02 08:11:02 +0100

    add multiple files with telescope to harpoon

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,23 @@
+-- this works https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+-- https://github.com/nvim-telescope/telescope.nvim/issues/814#issuecomment-1781603898
+-- https://github.com/nvim-telescope/telescope.nvim/pull/807#issuecomment-1703960904
+-- https://stackoverflow.com/questions/22142755/what-is-the-meaning-of-a-cr-at-the-end-of-some-vim-mappings
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        -- vim.cmd(string.format('%s %s', 'edit', j.path))
+        require("harpoon.mark").add_file(j.path)
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 return {
   {
     "telescope.nvim",
@@ -31,6 +51,13 @@ return {
           --     return math.floor(cols * 0.87)
           --   end
           -- end,
+        },
+        mappings = {
+          i = {
+            -- This allows the user to map <CR>. If multiple file selections are detected, it will edit/open each file. If hitting <CR> on a single selection, it'll fall back to actions.select_default...
+            -- select with Tab (goes up) or Shift-Tab.
+            ['<CR>'] = select_one_or_multi,
+          }
         },
       },
     },
