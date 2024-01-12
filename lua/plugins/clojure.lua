693be8a8e276c65160411f868e5977c217620829
Roman Frołow <rofrol@gmail.com>
2024-01-12 12:33:34 +0100

    clojure: cleaning

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -106,56 +106,6 @@ return {
       vim.g["conjure#mapping#doc_word"] = "K"
       vim.g["conjure#mapping#def_word"] = "gd"
     end,
-    dependencies = {
-      {
-        "PaterJason/cmp-conjure",
-        config = function()
-          local cmp = require("cmp")
-          local config = cmp.get_config()
-          table.insert(config.sources, {
-            name = "buffer",
-            option = {
-              sources = {
-                { name = "conjure" },
-              },
-            },
-          })
-          cmp.setup(config)
-        end,
-      },
-    },
-  },
-  -- {
-  --   "Olical/conjure",
-  --   ft = { "clojure", "fennel", "python" }, -- etc
-  --   -- [Optional] cmp-conjure for cmp
-  --   dependencies = {
-  --     {
-  --       "PaterJason/cmp-conjure",
-  --       config = function()
-  --         local cmp = require("cmp")
-  --         local config = cmp.get_config()
-  --         table.insert(config.sources, {
-  --           name = "buffer",
-  --           option = {
-  --             sources = {
-  --               { name = "conjure" },
-  --             },
-  --           },
-  --         })
-  --         cmp.setup(config)
-  --       end,
-  --     },
-  --   },
-  --   config = function()
-  --     require("conjure.main").main()
-  --     require("conjure.mapping")["on-filetype"]()
-  --   end,
-  --   init = function()
-  --     -- Set configuration options here
-  --     vim.g["conjure#debug"] = true
-  --   end,
-  -- },
   {
     "dundalek/parpar.nvim",
     dependencies = { "gpanders/nvim-parinfer", "julienvincent/nvim-paredit" },
