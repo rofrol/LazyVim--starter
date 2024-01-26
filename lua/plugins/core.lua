75030ac85a08b256d8a28bdaeacae75f9ec2da91
Roman Frołow <rofrol@gmail.com>
2024-01-26 14:31:52 +0100

    get back to zsh

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -148,8 +148,8 @@ return {
         shade_terminals = false,
         -- add --login so ~/.zprofile is loaded
         -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
-        -- shell = "zsh --login",
-        shell = "nu",
+        shell = "zsh --login",
+        -- shell = "nu",
       })
     end,
     keys = {
