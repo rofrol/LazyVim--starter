977afe0744f7ff7b98fe942666b7d375f94c8dbf
Roman Frołow <rofrol@gmail.com>
2024-05-24 02:56:50 +0200

    ccmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,11 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local ccmd = vim.api.nvim_create_autocmd
+
 -- override default LazyVim options for Markdown files
 -- FileType https://github.com/LazyVim/LazyVim/blob/879e29504d43e9f178d967ecc34d482f902e5a91/lua/lazyvim/config/autocmds.lua#L75
 -- vim.wo.conceallevel https://github.com/LazyVim/LazyVim/blob/879e29504d43e9f178d967ecc34d482f902e5a91/lua/lazyvim/plugins/extras/lang/tex.lua#L35
 -- https://github.com/ooloth/dotfiles/blob/6c3848ef4a13fe4be01e48108a883f4404d2f65e/config/nvim-lazyvim/lua/config/autocmds.lua#L26
-vim.api.nvim_create_autocmd('FileType', {
+ccmd('FileType', {
   group = vim.api.nvim_create_augroup("markdown_conceal", { clear = true }),
   pattern = { 'markdown', 'json' },
   callback = function()
@@ -14,7 +16,7 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
-vim.api.nvim_create_autocmd("TermOpen", {
+ccmd("TermOpen", {
   pattern = "*",
   callback = function()
     vim.wo.number = false
@@ -24,7 +26,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 -- does not work
 -- disable autoformat for markdown files as there is some truncation issue
 -- https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers
-vim.api.nvim_create_autocmd({ "FileType" }, {
+ccmd({ "FileType" }, {
   pattern = { "lua" },
   callback = function()
     vim.b.autoformat = false
@@ -33,7 +35,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 -- https://github.com/TristanLeclair/nvim/blob/8a6d3dfcdf01245b8fe34c5d9b1afffcfb063f56/lua/tlecla/options.lua#L46C1-L49C61
 -- https://vi.stackexchange.com/questions/3670/how-to-enter-insert-mode-when-entering-neovim-terminal-pane/22956#22956
-local ccmd = vim.api.nvim_create_autocmd
 -- ccmd("InsertEnter", { command = "set norelativenumber", pattern = "*" })
 -- ccmd("InsertLeave", { command = "set relativenumber", pattern = "*" })
 
