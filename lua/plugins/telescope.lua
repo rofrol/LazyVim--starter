fd8922537fa3bb0e6218c85d5aca3b4ee289975e
Roman Frołow <rofrol@gmail.com>
2024-05-28 21:59:32 +0200

    telescope: ripgrep exclude node_modules when searching

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,6 +27,58 @@ return {
         require("telescope").load_extension("zf-native")
       end,
     },
+    opts = {
+      defaults = {
+        -- does not ignore package-lock.json somehow
+        -- https://stackoverflow.com/questions/71809098/how-to-include-specific-hidden-file-folder-in-search-result-when-using-telescope/72545476#72545476
+        -- file_ignore_patterns = {
+        -- "node_modules", "build", "dist", "yarn.lock", ".angular", ".git", "package-lock.json", ".vscode"
+        -- },
+        -- ignores package-lock.json
+        -- https://stackoverflow.com/questions/71809098/how-to-include-specific-hidden-file-folder-in-search-result-when-using-telescope/76991432#76991432
+        vimgrep_arguments = {
+          "rg",
+          "--follow",        -- Follow symbolic links
+          -- "--hidden",        -- Search for hidden files
+          "--no-heading",    -- Don't group matches by each file
+          "--with-filename", -- Print the file path with the matched lines
+          "--line-number",   -- Show line numbers
+          "--column",        -- Show column numbers
+          "--smart-case",    -- Smart case search
+
+          -- Exclude some patterns from search
+          "--glob=!**/.git/*",
+          "--glob=!**/.idea/*",
+          "--glob=!**/.vscode/*",
+          "--glob=!**/build/*",
+          "--glob=!**/dist/*",
+          "--glob=!**/yarn.lock",
+          "--glob=!**/package-lock.json",
+          "--glob=!**/node_modules/*",
+          "--glob=!**/.angular/*",
+        },
+        pickers = {
+          -- does not ignore package-lock.json
+          find_files = {
+            hidden = true,
+            -- needed to exclude some files & dirs from general search
+            -- when not included or specified in .gitignore
+            find_command = {
+              "rg",
+              "--files",
+              "--hidden",
+              "--glob=!**/.git/*",
+              "--glob=!**/.idea/*",
+              "--glob=!**/.vscode/*",
+              "--glob=!**/build/*",
+              "--glob=!**/dist/*",
+              "--glob=!**/yarn.lock",
+              "--glob=!**/package-lock.json",
+            },
+          },
+        },
+      }
+    }
   },
   {
     "nvim-telescope/telescope.nvim",
