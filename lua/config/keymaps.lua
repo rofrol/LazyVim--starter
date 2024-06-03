cac983b82bfface2f8b76f854f9b6b27c3a6be0c
Roman Frołow <rofrol@gmail.com>
2024-06-03 11:10:39 +0200

    cleaning

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local wk = require("which-key")
-
 -- https://vi.stackexchange.com/questions/39149/how-to-stop-neovim-from-yanking-text-on-pasting-over-selection/39907#39907
 -- local function map(m, k, v)
 --   vim.keymap.set(m, k, v, { silent = true })
