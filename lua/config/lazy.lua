d87ac2b26b9f981a294afeda30eea738090fedd3
Roman Frołow <rofrol@gmail.com>
2024-06-14 17:20:35 +0200

    lazy: disable some plugins but why?

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,14 +52,14 @@ require("lazy").setup({
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        "gzip",
+        -- "gzip",
         -- "matchit",
         -- "matchparen",
         -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
       },
     },
   },
