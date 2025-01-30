f13cfc5df83b61d01a9459b8cd32758bd83b6d5f
Roman Frołow <rofrol@gmail.com>
2025-01-30 17:26:00 +0100

    mini.files preview

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -646,4 +646,20 @@ return {
         require("telescope").load_extension("oil")
     end
   },
+  { 'echasnovski/mini.files', version = '*',
+    opts = {
+      windows = {
+        -- Maximum number of windows to show side by side
+        max_number = math.huge,
+        -- Whether to show preview of file/directory under cursor
+        preview = true,
+        -- Width of focused window
+        width_focus = 50,
+        -- Width of non-focused window
+        width_nofocus = 15,
+        -- Width of preview window
+        width_preview = 50,
+      },
+    },
+  },
 }
