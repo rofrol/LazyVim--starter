87863c81d38a8560f65376fcf820ee2ad4611ef9
Roman Frołow <rofrol@gmail.com>
2025-10-17 01:01:51 +0200

    move tailwind from core to tailwind.lua and disable it for now

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -716,26 +716,6 @@ return {
     },
   },
   {
-    "luckasRanarison/tailwind-tools.nvim",
-    name = "tailwind-tools",
-    build = ":UpdateRemotePlugins",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim", -- optional
-      "neovim/nvim-lspconfig", -- optional
-    },
-    opts = {
-      conceal = {
-        enabled = true, -- can be toggled by commands
-        min_length = nil, -- only conceal classes exceeding the provided length
-        symbol = "󱏿", -- only a single character is allowed
-        highlight = { -- extmark highlight options, see :h 'highlight'
-          fg = "#38BDF8",
-        },
-      },
-    } -- your configuration
-  },
-  {
     "gpanders/editorconfig.nvim",
     lazy = false,
   }
---
+++ lua/plugins/tailwinds.lua
@@ -0,0 +1,24 @@
+-- disabled, because errors
+if true then return {} end
+return {
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {
+      conceal = {
+        enabled = true, -- can be toggled by commands
+        min_length = nil, -- only conceal classes exceeding the provided length
+        symbol = "󱏿", -- only a single character is allowed
+        highlight = { -- extmark highlight options, see :h 'highlight'
+          fg = "#38BDF8",
+        },
+      },
+    }
+  },
+}
